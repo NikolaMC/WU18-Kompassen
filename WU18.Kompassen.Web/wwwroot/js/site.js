90fd1401bbfe230c60410cd7994da45097004778
Alexander Marin <maal18en@student.ju.se>
2018-11-27 11:05:06 +0100

    table på kurser, separat javascript fil

--- WU18.Kompassen.Web/wwwroot/js/site.js
+++ WU18.Kompassen.Web/wwwroot/js/site.js
@@ -1,4 +1,87 @@
-﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
-// for details on configuring this project to bundle and minify static web assets.
+﻿$("#panel2").hide();
+$("#panel3").hide();
+$("#navKurser").on("click", function () {
 
-// Write your JavaScript code.
+
+
+    $("#panel1").show();
+    $("#panel2").hide();
+    $("#panel3").hide();
+
+
+
+
+
+});
+$("#navAddNewStudent").on("click", function () {
+
+
+
+    $("#panel2").hide();
+    $("#panel1").hide();
+    $("#panel3").show();
+
+
+});
+$("#navStudenter").on("click", function () {
+
+
+
+    $("#panel2").show();
+    $("#panel1").hide();
+    $("#panel3").hide();
+
+
+});
+
+/* $("#navNyKurs").on("click", function () {
+
+
+
+     $("#panel3").show();
+     $("#panel1").hide();
+     $("#panel2").hide();
+
+
+ });*/
+
+$("#kurs_Slider").on("click", function () {
+
+
+
+    $("#panel3").slideToggle();
+
+    var icon = "<span class='glyphicon glyphicon-plus' aria-hidden='true'>";
+    var $this = $(this);
+    $this.toggleClass('open');
+
+    var option1 = icon + ' Lägg till ny kurs';
+    var option2 = ' Avbryt';
+
+    if ($this.hasClass('open')) {
+        $this.html(option1);
+    } else {
+        $this.html(option2);
+    }
+
+});
+
+$("#btnClose").on("click", function () {
+
+
+
+    $("#panel3").slideUp();
+
+
+    $("#kurs_Slider").AddClass('open');
+
+    //funkar inte
+
+    if (!$('#kurs_Slider').hasClass('open')) {
+        $('#kurs_Slider').addClass('open');
+    }
+
+
+
+
+});
