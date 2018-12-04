111f354cc6c363e111e043ce9aa939c246ff67a6
Alexander Marin <maal18en@student.ju.se>
2018-12-04 14:52:58 +0100

    massa grejer

--- WU18.Kompassen.Web/wwwroot/js/site.js
+++ WU18.Kompassen.Web/wwwroot/js/site.js
@@ -12,6 +12,9 @@ $('#removeStudentSuccessMessage').hide();
 $('#registeredStudentSuccessMessage').hide();
 $('#courseCreatedSuccessMessage').hide(700);
 $('#editButtonDropdown').hide();
+$('#updateStudent').hide();
+
+$('#updatedStudentSuccessMessage').hide();
 
 
 
@@ -273,14 +276,25 @@ $courseTable.on("click", ".saveEdit", function () {
 
 var $dropdownListStudents = $("#dropdownListStudents");
 
+
 $.get("/api/students", function (data) {
+
+
     //console.log(data);
     $.each(data, function (i, students) {
 
-        $dropdownListStudents.append('<li class="dropdowns"><a href="#">' + students.firstName + '</a>'+ ' ' + students.lastName + '</li>');
+        getStudentsInDropdown(students)
 
     });
+
 });
+
+function getStudentsInDropdown(students) {
+        $dropdownListStudents.append('<li class="dropdowns"><a href="#">' + students.firstName + '</a>' + ' ' + students.lastName + '</li>');
+}
+
+
+
 
 var $dropdownListCourses = $("#dropdownListCourses");
 
@@ -327,8 +341,8 @@ $.get("/api/courses", function (courses) {
 
 });
 
-var studentIsSelceted = false; 
-var courseIsSelceted = false; 
+var studentIsSelceted = false;
+var courseIsSelceted = false;
 var selectedCourseNameStudentsArray;
 
 
@@ -353,102 +367,103 @@ $(document).on("click", "li.dropdownCourses a", function (e) {
                 }
             });
         }
-    var studentIsSelceted = false;        
+        var studentIsSelceted = false;
     });
 });
 
 
+var clickedStudentName = null;
 
 
 $(document).on("click", "li.dropdowns a", function (e) {
     e.preventDefault(); // Eftersom vi klickar på a tagg som har en ankar länkar (#) så säger vi skit i o följa länken.
-    var clickedStudentName = $(this).html();
+    clickedStudentName = $(this).html();
     console.log(clickedStudentName);
 
     $('#displaySelectedStudent').empty();
-    $('#editButtonDropdown').fadeIn(1700);
+    $('#editButtonDropdown').fadeIn(700);
 
-     studentIsSelceted = true;
+    studentIsSelceted = true;
 
-        if (studentIsSelceted === true) {
-            $('#dropdownMenuButtonStudents').empty();
-            $('#dropdownMenuButtonStudents').append(clickedStudentName);
-            
-            $('#addStudentsEventButton').on("click", function (e) {
-                var sendingStudentId = null;
-                $.get("/api/searchstudents/" + clickedStudentName, function (student) {
-                  
+    if (studentIsSelceted === true) {
+        $('#dropdownMenuButtonStudents').empty();
+        $('#dropdownMenuButtonStudents').append(clickedStudentName);
 
-                    
-                    console.log(student);
-                    
-                    
-                    console.log('Id som skickas med: ' + sendingStudentId);
-                    $.each(student, function (i, studendata) {
-
-
-                      
-
-                        var studentId = studendata;
-
-
-                        sendingStudentId = studentId.id;
+        $('#addStudentsEventButton').on("click", function (e) {
+            var sendingStudentId = null;
+            $.get("/api/searchstudents/" + clickedStudentName, function (student) {
 
 
 
-                    });
-                            
-                            var postStudent = {
+                console.log(student);
 
-                                courseId: selectedCourseId,
-                                studentId: sendingStudentId
 
-                            };
-                        
-                    var courseJSON = JSON.stringify(postStudent);
-                    console.log(courseJSON);
+                console.log('Id som skickas med: ' + sendingStudentId);
+                $.each(student, function (i, studendata) {
 
-                            
-                            $.ajax({
-                                type: "POST",
-                                contentType: 'application/json',
-                                url: "/api/studentcourses/",
-                                dataType: "json",
-                                data: courseJSON,
-                                success: function (Course) {
 
-                                    $("#panelGenerator .row").empty();
-                                    console.log('Panel generator empty');
-                                    $('#registeredStudentSuccessMessage').fadeIn(700);
-                                    $('#registeredStudentSuccessMessage').fadeOut(7000);
 
-                                    $.get("/api/courses", function (courses) {
-                                        getStudentsAndCourses(courses);
 
-                                    });
-                                }
+                    var studentId = studendata;
 
-                            });
+
+                    sendingStudentId = studentId.id;
+
+
+
                 });
 
-            });
-       
+                var postStudent = {
 
-        var studentIsSelceted = false; 
-};
+                    courseId: selectedCourseId,
+                    studentId: sendingStudentId
+
+                };
+
+                var courseJSON = JSON.stringify(postStudent);
+                console.log(courseJSON);
+
+
+                $.ajax({
+                    type: "POST",
+                    contentType: 'application/json',
+                    url: "/api/studentcourses/",
+                    dataType: "json",
+                    data: courseJSON,
+                    success: function (Course) {
+
+                        $("#panelGenerator .row").empty();
+                        console.log('Panel generator empty');
+                        $('#registeredStudentSuccessMessage').fadeIn(700);
+                        $('#registeredStudentSuccessMessage').fadeOut(7000);
+
+                        $.get("/api/courses", function (courses) {
+                            getStudentsAndCourses(courses);
+
+                        });
+                    }
+
+                });
+            });
+
+        });
+
+
+        var studentIsSelceted = false;
+    };
 });
 
 
 
 function resetDropdowns() {
-    
-        $('#dropdownMenuButtonStudents').empty();
-        $('#dropdownMenuButtonStudents').append(' <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>  Studenter');
 
-        $('#dropdownMenuButtonCourses').empty();
-        $('#dropdownMenuButtonCourses').append(' <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>  Studenter');
+    $('#dropdownMenuButtonStudents').empty();
+    $('#dropdownMenuButtonStudents').append(' <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>  Studenter');
 
-   
+    $('#dropdownMenuButtonCourses').empty();
+    $('#dropdownMenuButtonCourses').append(' <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>  Studenter');
+
+
 
 }
 
@@ -470,11 +485,11 @@ var createStudentInput_Active;
 
 
 $('#createStudentsAbort').on("click", function () {
-    
+
     document.getElementById('createStudentInput_Name').value = '';
     document.getElementById('createStudentInput_LastName').value = '';
     document.getElementById('createStudentInput_SSN').value = '';
-   
+
 });
 
 
@@ -486,7 +501,7 @@ $('#createNewStudentBtn').on("click", function () {
 
     var checkBox = $('#createStudentInput_Active');
 
-    if (checkBox.checked == true) {
+    if (checkBox.is(":checked")) {
         createStudentInput_Active = true;
     } else {
         createStudentInput_Active = false;
@@ -502,6 +517,8 @@ $('#createNewStudentBtn').on("click", function () {
 
     };
 
+  
+
 
 
 
@@ -514,7 +531,7 @@ $('#createNewStudentBtn').on("click", function () {
         dataType: "json",
         data: newStudentJSON,
         success: function (Course) {
-           
+
             createStudentInput_Name.empty();
             createStudentInput_LastName.empty();
             createStudentInput_SSN.empty();
@@ -531,20 +548,20 @@ $('#createNewStudentBtn').on("click", function () {
             $.get("/api/students", function (data) {
                 //console.log(data);
                 $.each(data, function (i, students) {
-                   
+
                     $dropdownListStudents.append('<li class="dropdowns"><a href="#">' + students.firstName + ' ' + students.lastName + '</a></li>');
 
                 });
             });
 
 
-           
+
         }
 
     });
 
 
-    
+
 });
 
 
@@ -555,34 +572,30 @@ $('#avregistreraStudent').on("click", function () {
     $('#removeStudentSuccessMessage').fadeOut(7000);
 });
 
-
+var studentFirstNameForInput = null;
+var studentLastNameForInput = null;
+var studentSSNForInput = null;
+var studentActiveForInput;
 
 $('#editButtonDropdown').on("click", function () {
 
-   
-   // studentActiveForInput;
+    $('#updateStudent').fadeIn(300);
     
-   
+
+
     $.get("/api/searchstudents/" + clickedStudentName, function (student) {
-
-
-
-        console.log(student);
 
 
         $.each(student, function (i, studendata) {
 
-
-           var studentFirstNameForInput = studentdata.firstName;
-           var studentLastNameForInput = studentdata.lastName;
-           var studentSSNForInput = studentdata.ssn;
-           var studentActiveForInput = studentdata.active;
+            var studentdata1 = studendata;
 
 
-            var studentId = studendata;
-
-
-            sendingStudentId = studentId.id;
+            studentId = studentdata1.id;
+            studentFirstNameForInput = studentdata1.firstName;
+            studentLastNameForInput = studentdata1.lastName;
+            studentSSNForInput = studentdata1.ssn;
+            studentActiveForInput = studentdata1.active;
 
 
 
@@ -590,12 +603,91 @@ $('#editButtonDropdown').on("click", function () {
 
         document.getElementById('createStudentInput_Name').value = studentFirstNameForInput;
         document.getElementById('createStudentInput_LastName').value = studentLastNameForInput;
-        document.getElementById('createStudentInput_SSN').value = stustudentSSNForInputdentSSN;
+        document.getElementById('createStudentInput_SSN').value = studentSSNForInput;
+        console.log(studentActiveForInput);
 
-        
+        if (studentActiveForInput == true) {
+
+            $("#createStudentInput_Active").attr("checked", "checked");
+        } else {
+            $('#createStudentInput_Active').prop('checked', false);
+        }
+
+        //document.getElementById('createStudentInput_Active') = studentActiveForInput;
+
+
+    });
 
 });
 
 
- 
+
+$('#updateStudentEventButton').on("click", function () {
+
+    var createStudentInput_Active;
+
+    if ($('#createStudentInput_Active').is(":checked")) {
+        createStudentInput_Active = true;
+    } else {
+        createStudentInput_Active = false;
+    }
+
+    var firstNameInput = $('#createStudentInput_Name').val();
+       var lastNameInput = $('#createStudentInput_LastName').val();
+       var studentSSNInput = $('#createStudentInput_SSN').val();
+
+    var postStudent = {
+        id: studentId,
+        firstName: firstNameInput,
+        lastName: lastNameInput,
+        ssn: studentSSNInput,
+        active: createStudentInput_Active
+    };
+
+    var postStudent = JSON.stringify(postStudent);
+
+
+    $.ajax({
+        type: "PUT",
+        url: "/api/students/" + studentId,
+        dataType: "json",
+        contentTyp: "application/json",
+        data: postStudent,
+        success: function () {
+            
+
+            $('#updatedStudentSuccessMessage').fadeIn(700);
+            $('#updatedStudentSuccessMessage').fadeOut(7000);
+
+            $dropdownListStudents.empty();
+
+            $.get("/api/students", function (data) {
+
+
+                //console.log(data);
+                $.each(data, function (i, students) {
+
+                    getStudentsInDropdown(students)
+
+                });
+
+            });
+
+
+           
+        }
+    });
+
+    document.getElementById('createStudentInput_Name').value = '';
+    document.getElementById('createStudentInput_LastName').value = '';
+    document.getElementById('createStudentInput_SSN').value = '';
+
+    $('#dropdownMenuButtonStudents').empty();
+    $('#dropdownMenuButtonStudents').append(' <span class="glyphicon glyphicon-chevron-down" aria-hidden="true"></span>  Studenter');
+
+    $('#editButtonDropdown').hide();
+
+});
+
+
 
